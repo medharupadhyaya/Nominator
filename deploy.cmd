188c8d04ac87d36ac43a013a081a9d57f1f95f32
Vikas Shivakumar <m1041679@mindtree.com>
2018-05-18 10:50:27 +0530

    work

--- deploy.cmd
+++ deploy.cmd
@@ -100,8 +100,8 @@ IF EXIST "%DEPLOYMENT_SOURCE%\package.json" (
 )
 
 :: 3. Angular Prod Build
-IF EXIST "%DEPLOYMENT_SOURCE%/.angular.json" (
-echo Building App in %DEPLOYMENT_SOURCE%…
+IF EXIST "%DEPLOYMENT_SOURCE%/angular.json" (
+echo Building App in %DEPLOYMENT_SOURCE%
 pushd "%DEPLOYMENT_SOURCE%"
 call :ExecuteCmd !NPM_CMD! run build
 :: If the above command fails comment above and uncomment below one
